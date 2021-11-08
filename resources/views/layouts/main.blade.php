496a70d93047a17ff7aa3a10d9c0412ba3a7445c
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-11-08 08:22:58 +0100

    All Features Working Correctly

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,31 +14,32 @@
     <nav class="border-b border-gray-800">
         <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
             <ul class="flex flex-col md:flex-row items-center">
-                <li>
-                    <a href="{{ route('movies.index') }}">Logo</a>
-                    <!-- My Logo -->
-                </li>
-                <li class="md:ml-6 md:ml-4 mt-3 md:mt-0">
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="{{ route('movies.index') }}" class="hover:text-gray-300">Movies</a>
                 </li>
-                <li class="md:ml-6 md:ml-4 mt-3 md:mt-0">
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="{{ route('tv.index') }}" class="hover:text-gray-300">Tv Shows</a>
                 </li>
-                <li class="md:ml-6 md:ml-4 mt-3 md:mt-0">
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="{{ route('actors.index') }}" class="hover:text-gray-300">Actors</a>
                 </li>
             </ul>
             <div class="flex flex-col md:flex-row items-center">
                 <livewire:search-dropdown>
                 <div class="md:ml-4 mt-3 md:mt-0">
-                    <a href="#">
-                        <img src="/img/avatar.jpg" alt="Avatar" class="rounded-full w-8 h-8">
+                    <a href="http://adeyinka.000webhostapp.com" target="_blank">
+                        <img src="/img/avatar.png" alt="Avatar" class="rounded-full w-8 h-8">
                     </a>
                 </div>
             </div>
         </div>
     </nav>
     @yield('content')
+    <footer class="border border-t border-gray-800">
+        <div class="container mx-auto text-sm px-4 py-6">
+            Powered by <a href="https://www.themoviedb.org/documentation/api" class="underline hover:text-gray-300">TMDb API</a>
+        </div>
+    </footer>
     <livewire:scripts>
     @yield('scripts')
 </body>
